99b4425b84ad629a5296a1d2a49defb14ac9cf5d
Barri <info@bdesignstudio.co.uk>
2020-11-06 16:56:17 +0000

    set up time out for input and fetching api

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hi there!');
+
+//96326c24
+//http://www.omdbapi.com/?apikey=[yourkey]&
+
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '96326c24',
+      s: searchTerm
+      // i: 'tt08488'
+      //t: 'Batman'
+    }
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+let timeoutId;
+
+const onInput = e =>{
+  if(timeoutId){
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(()=>{
+    fetchData(e.target.value);
+  }, 1000)
+  
+}
+input.addEventListener('input', onInput)
