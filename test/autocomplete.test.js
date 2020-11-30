3391128c1060bf5ec03fe010d96fa05d921bc6d1
Barri <info@bdesignstudio.co.uk>
2020-11-30 19:23:36 +0000

    implement waitFor function

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -3,6 +3,24 @@
 
 // });
 //--setup env before each test 
+//-------- Function for waiting the code execute then run the test-- it will return a promise
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(timeout);
+        resolve();
+      }
+    }, 30);
+    clearTimeout()
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 2000);
+  });
+}
+//---------
 beforeEach(() => {
   document.querySelector('#target').innerHTML = '';//deleting before any autocomplete 
   createAutoComplete({
@@ -45,14 +63,14 @@ it('Shows an autocomplete!Dropdown Starts closed', () => {
   expect(dropdown.className).not.to.include('is-active')
 });
 
-it('After searching , dropdown opens up', () => {
+it('After searching , dropdown opens up', async () => {
   //type something in 
   const input = document.querySelector('input');
   input.value = 'batman'
   input.dispatchEvent(new Event('input'));
 
   await waitFor('.dropdown-item');
-  
+
   const dropdown = document.querySelector('.dropdown');
 
   expect(dropdown.className).to.include('is-active')
